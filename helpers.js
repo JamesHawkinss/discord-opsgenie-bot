640f7db7d59e5fa8fc5703d894497efcbe50dd48
James Hawkins <jhawki2005@gmail.com>
2020-07-31 22:44:04 +0100

    that's enough for tonight

--- helpers.js
+++ helpers.js
@@ -2,15 +2,21 @@ const discord = require('discord.js');
 const opsgenie = require('opsgenie-sdk');
 const config = require('./config');
 const { response } = require('express');
+const e = require('express');
 
 opsgenie.configure({
     "api_key": config.opsgenie.api_key
 });
 
+function createEmbed() {
+    const embed = new discord.MessageEmbed();
+    return embed;
+}
+
 async function sendOpsgenieEmbed(data, channel) {
     if (data.action !== "Create") return;
 
-    const embed = new discord.MessageEmbed();
+    const embed = createEmbed();
     embed.setTitle("New OpsGenie Alert");
 
     if (data.alert.message) embed.addField("Message", data.alert.message);
@@ -22,38 +28,79 @@ async function sendOpsgenieEmbed(data, channel) {
         });
         embed.addField("Assigned Teams", teams);
     }
-    if (data.alert.recipients.length > 0) {
-        let recipients = "";
-        data.alert.recipients.forEach((recipient) => {
-            recipients += `${recipient}\n`;
-        });
-        embed.addField("Assigned Recipients", recipients);
+    if (data.alert.recipients) {
+        console.log(data.alert.recipients);
+        // let recipients = "";
+        // data.alert.recipients.forEach((recipient) => {
+        //     recipients += `${recipient}\n`;
+        // });
+        // embed.addField("Assigned Recipients", recipients);
     }
-    if (data.alert.alertId && data.alert.tinyId) embed.setFooter(`${data.alert.alertId}${tinyId}`);
+    if (data.alert.alertId && data.alert.tinyId) embed.setFooter(`${data.alert.alertId}`);
 
     const message = await channel.send(embed);
     return message;
 }
 
-async function closeOpsgenieAlert(id) {
+async function closeOpsgenieAlert(id, username) {
+    var out;
+
     const identifier = {
         identifier: id,
         identifierType: 'id'
     };
 
     const closeData = {
-        "note": "Closed by discord-opsgenie-bot",
+        "note": `Closed by Discord user "${username}"`,
         "source": "discord-opsgenie-bot"
     };
 
-    opsgenie.alertV2.close(identifier, closeData, (error, result) => {
-        if (error) throw error;
-        console.log(response);
-        return response;
-    });
+    try {
+        opsgenie.alertV2.close(identifier, closeData, (error, result) => {
+            if (error) throw error;
+            out = result;
+        });
+    
+        return out;
+    } catch (e) {
+        throw e;
+    }
+}
+
+async function ackOpsgenieAlert(id, username) {
+    var out;
+
+    const identifier = {
+        identifier: id,
+        identifierType: 'id'
+    };
+
+    const ackData = {
+        "note": `Closed by Discord user "${username}"`,
+        "source": "discord-opsgenie-bot"
+    };
+
+    try {
+        opsgenie.alertV2.acknowledge(identifier, ackData, (error, result) => {
+            if (error) throw error;
+            out = result;
+        });
+
+        return out;
+    } catch (e) {
+        throw e;
+    }
+}
+
+async function getRelatedMessages(channel, id) {
+    let messages = await channel.messages.fetch();
+    messages = messages.filter((msg) => msg.embeds[0].footer.text == id);
+    return messages;
 }
 
 module.exports = {
+    createEmbed,
     sendOpsgenieEmbed,
-    closeOpsgenieAlert
+    closeOpsgenieAlert,
+    getRelatedMessages
 }
--- index.js
+++ index.js
@@ -10,27 +10,77 @@ app.use(bodyparser.json())
 
 const client = new discord.Client();
 
-opsgenie.configure({
-    "api_key": config.opsgenie.api_key
-});
+app.post('/opsgenie', async (req, res) => {
+    res.sendStatus(200);
+    const data = req.body;
+    console.log(data);
+    const channel = await client.channels.cache.get(config.discord.channel);
 
-app.get('/opsgenie', async (req, res) => {
-    const channel = client.channels.cache.get(config.discord.channel);
-    const message = await helpers.sendOpsgenieEmbed(req.body, channel);
+    if (data.action == "Create") {
+        const message = await helpers.sendOpsgenieEmbed(data, channel);
     
-    // react to message with emojis
+        Object.keys(config.emojis).forEach(async (key) => {
+            await message.react(message.guild.emojis.cache.get(config.emojis[key]));
+        });
+    } else if (data.action == "Close") {
+        const messages = await helpers.getRelatedMessages(channel, data.alert.alertId);
+        
+        if (messages.array().length > 0) {
+            messages.each((message) => message.delete());
+        }
+    } else if (data.action == "Acknowledge") {
+        const messages = await helpers.getRelatedMessages(channel, data.alert.alertId);
+
+        if (messages.array().length > 0) {
+            messages.each(async (message) => {
+                let reaction = message.reactions.cache.get(config.emojis.acknowledge);
+                reaction = await message.reactions.resolve(reaction);
+                reaction.remove();
+                const embed = message.embeds[0];
+                embed.addField("Acknowledged", "True");
+                message.edit(embed);
+            })
+        }
+    } else if (data.action == "UnAcknowledge") {
+        const messages = await helpers.getRelatedMessages(channel, data.alert.alertId);
+
+        if (messages.array().length > 0) {
+            messages.each(async (message) => {
+                let reaction = message.reactions.cache.get(config.emojis.acknowledge);
+                message.react(reaction);
+                const embed = message.embeds[0];
+                embed.addField("Acknowledged", "False");
+                message.edit(embed);
+            })
+        }
+    }
 });
 
-client.on('messageReactionAdd', (reaction, user) => {
+client.on('messageReactionAdd', async (reaction, user) => {
     if (reaction.message.author.id !== client.user.id) return;
     if (reaction.message.channel.id !== config.discord.channel) return;
+    if (reaction.count <= 1) return;
 
     const alertId = reaction.message.embeds[0].footer;
 
-    if (reaction.id == config.emojis.acknowledge) {
-        // acknowledge alert
+    if (reaction.emoji.id == config.emojis.acknowledge) {
+        try {
+            reaction.remove();
+            const result = await helpers.ackOpsgenieAlert(alertId, username);
+            console.log(result);
+        } catch (e) {
+            reaction.message.channel.send(`Failed to acknowledge alert!\nError: ${e.name} ${e.message}`);
+        }
+    } else if (reaction.emoji.id == config.emojis.close) {
+        try {
+            reaction.remove();
+            const result = await helpers.closeOpsgenieAlert(alertId, user.username);
+            console.log(result);
+        } catch (e) {
+            reaction.message.channel.send(`Failed to close alert!\nError: ${e.name} ${e.message}`);
+        }
     }
-})
+});
 
 app.listen(3000, () => console.log("express live"));
 client.on('ready', () => console.log("discord live"));
